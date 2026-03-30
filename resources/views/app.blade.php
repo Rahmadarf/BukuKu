ba70b8da0a925154f75eb611de8ed69f06dfd33e
Rahmad Arifin Susilo <Kumar@Macs-MacBook-Air.local>
2026-03-30 22:42:28 +0700

    page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,10 +8,12 @@
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
+    @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
+
 <body>
     @inertia
 </body>
+
 </html>
